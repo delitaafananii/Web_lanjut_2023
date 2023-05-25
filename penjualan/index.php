2261ac10ea23551ca7562326b88725293663650d
Delitaaaa <93269071+delitaafananii@users.noreply.github.com>
2023-05-25 08:25:41 +0700

    Add files via upload

---
+++ penjualan/index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Web Responsive</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+     
+    <div class="badan-utama">
+        
+        <header>
+            <center>
+                <h2>KELOMPOK</h2>
+                <p>Warung Masakan Arab Saudi</p>
+            </center>
+
+        </header>
+ 
+        <nav class="navigasi">
+            <ul>
+                <li><a class="active" href="#home">Home</a></li>
+                <li><a href="#berita">Menu</a></li>
+                <li><a href="#kontak">About</a></li>
+                <li><a href="#tentang">Store</a></li>
+            </ul>
+        </nav>
+ 
+        <div class="banner">
+        </div>
+ 
+        <div class="menu-kiri">
+            <div class="kotak">
+                <h3>Pemesanan</h3>
+                <p>
+                    Isi Form Pemesanan dengan Klik Tombol di bawah ini
+                </p>
+ 
+                <a class="tombol tombol-pesan" href="#">Pesan</a>
+            </div>
+        </div>
+ 
+        <div class="menu-tengah">
+            <div class="kotak">
+                <h3>Sate Kambing Arabia</h3>
+                <span class="tanggal-posting">
+                </span>
+ 
+                <img src="img/a.jpg" alt="Sate Ayam">
+ 
+                <p>
+                    Sate kambing arabia adalah sate yang memiliki kalori paling tinggi yang mengandung energi sebesar 211 kkal tetapi daging kambing masih termasuk dalam makanan yang kandungan nutrisi. jika dikonsumsi dengan berlebihan, bisa menyebabkan dampak yang kurang baik seperti kolesterol tinggi, kenaikan berat badan, hingga diabetes.
+
+                </p>
+ 
+                <a class="tombol tombol-lengkap" href="#">Selengkapnya</a>
+            </div>
+        </div>
+ 
+        <div class="menu-kanan">
+            <div class="kotak">
+                <h3>Jadwal Buka</h3>
+                <img src="" alt="">
+ 
+                <h4 align="center">Jadwal Buka</h4>
+                <center>
+                    <p>Senin-Minggu 08.00 WIB - 22.00 WIB</p>
+                </center>
+            </div>
+ 
+ 
+            <div class="kotak">
+                <h3>Berita Terbaru</h3>
+ 
+                <nav class="menu-artikel">
+                    <ul>
+                        <img src="img/c.jpg" alt="nasi kebuli">
+                        <li><a href="#">Nasi Kebuli Arabia</a></li>
+                        <img src="img/c.jpg" alt="nasi kebuli">
+                        <li><a href="#">Menu Makanan Keluarga</a></li>
+                        <li><a href="#">Sejarah pembuatan sambal</a></li>
+                        <li><a href="#">Minuman pelepas dahaga</a></li>
+                        <li><a href="#">Ayam Geprek Gurih</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+ 
+        <footer>
+            
+        </footer>
+ 
+    </div>
+ 
+</body>
+</html>
